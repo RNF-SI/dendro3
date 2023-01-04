ca29b21e0cad4108edc63b019f7c8bb2151e1308
aschlegle <antoine.schlegel01@gmail.com>
2023-01-04 18:38:46 +0100

    test commit - remove comment

--- lib/presentation/view/user_dispositif_list.dart
+++ lib/presentation/view/user_dispositif_list.dart
@@ -9,39 +9,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UserDispositifList extends ConsumerWidget {
-  // <2>
   const UserDispositifList({Key? key}) : super(key: key);
-
-  // @override
-  // ConsumerState<UserDispositifList> createState() =>
-  //     _UserDispositifListState(); // <3>
-// }
-
-// class _UserDispositifListState extends ConsumerState<UserDispositifList> {
-  // <4>
-  // late DispositifList fistDispList;
-
-  // @override
-  // void initState() {
-  //   super.initState();
-
-  //   // _downloadControllers = List<DownloadController>.generate(
-  //   //   20,
-  //   //   (index) => SimulatedDownloadController(onOpenDownload: () {
-  //   //     _openDownload(index);
-  //   //   }),
-  //   // );
-
-  //   // fistDispList = ref.read(userDispositifListViewModelStateNotifierProvider); // <5>
-  // }
-
-  // void _openDownload(int index) {e
-  //   ScaffoldMessenger.of(context).showSnackBar(
-  //     SnackBar(
-  //       content: Text('Open App ${index + 1}'),
-  //     ),
-  //   );
-  // }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -49,31 +17,14 @@ class UserDispositifList extends ConsumerWidget {
     final userDispositifList =
         ref.watch(userDispositifListViewModelStateNotifierProvider);
 
-    // final modelList = ref.watch(containerListProvider);
-
-    // return
-    // Expanded(
-    // child: const Text('Uh o'),
-    // child:
     return _userDispositifListProvider.maybeWhen(
       success: (data) => _buildDispositifListWidget(context, data),
-      // success: (data) => const Text('Uh oh... Something went wrong...',
-      //     style: TextStyle(color: Colors.white)),
-
       error: (_) => const Center(
         child: Text('Uh oh... Something went wrong...',
             style: TextStyle(color: Colors.white)),
       ),
       orElse: () =>
           const Expanded(child: Center(child: CircularProgressIndicator())),
-
-      // data: (data) => _buildDispositifListWidget(context, data),
-      // loading: () => const Center(child: CircularProgressIndicator()),
-      // error: (error, _) => const Center(
-      //   child: Text('Uh oh... Something went wrong...',
-      //       style: TextStyle(color: Colors.white)),
-      // ),
-      // ),
     );
   }
 
@@ -99,16 +50,10 @@ class UserDispositifList extends ConsumerWidget {
 class DispositifItemCardWidget extends ConsumerWidget {
   const DispositifItemCardWidget({
     Key? key,
-    // required this.context,
     required this.dispositifInfo,
   }) : super(key: key);
-  // Widget _buildDispositifItemCardWidget(
-  // final BuildContext context, final Dispositif dispositif,final Model this.model) {
 
-  // final downloadController = _downloadControllers[index];
-  // final Model model;
   final DispositifInfo dispositifInfo;
-  // final BuildContext context;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -128,10 +73,6 @@ class DispositifItemCardWidget extends ConsumerWidget {
                       style: Theme.of(context).textTheme.headline6,
                       overflow: TextOverflow.ellipsis,
                     ),
-                    // Text(
-                    //   DateFormat('yyyy/MM/dd').format(dispositif.dueDate),
-                    //   style: Theme.of(context).textTheme.caption,
-                    // ),
                     const SizedBox(height: 4),
                     Text(
                       dispositifInfo.dispositif.idOrganisme.toString(),
@@ -148,44 +89,14 @@ class DispositifItemCardWidget extends ConsumerWidget {
                   ],
                 ),
               ),
-              // IconButton(
-              //   iconSize: 72,
-              //   icon: Icon(Icons.favorite),
-              //   onPressed: () {
-              //     ref
-              //         .read(userDispositifListViewModelStateNotifierProvider
-              //             .notifier)
-              //         .downloadData(3);
-              //     // userDispositifList.downloadData();
-              //   },
-              // ),
               SizedBox(
                 width: 96,
                 child: DownloadButton(dispInfo: dispositifInfo),
-                // AnimatedBuilder(
-                //   animation: downloadController,
-                //   builder: (context, child) {
-                //     return DownloadButton(
-                //       status: downloadController.downloadStatus,
-                //       downloadProgress: downloadController.progress,
-                //       onDownload: downloadController.startDownload,
-                //       onCancel: downloadController.stopDownload,
-                //       onOpen: downloadController.openDownload,
-                //     );
-                //   },
-                // ),
               ),
-              // const SizedBox(width: 8),
-              // dispositif.isCompleted ? _buildCheckedIcon(context, dispositif) : _buildUncheckedIcon(context, dispositif),
             ],
           ),
         ),
       ),
-      // onTap: () => Navigator.push(
-      //     context,
-      //     MaterialPageRoute(
-      //       builder: (_) => DispositifFormPage(dispositif),
-      //     )),
     );
   }
 }
