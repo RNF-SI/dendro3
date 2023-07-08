d1922ec97b934b764d6945c7c1a83d2782559163
aschlegle <antoine.schlegel01@gmail.com>
2023-07-08 15:06:58 +0200

    remove useless code

--- lib/domain/repository/arbres_repository.dart
+++ lib/domain/repository/arbres_repository.dart
@@ -1,8 +1,4 @@
-import 'package:dendro3/data/entity/arbres_entity.dart';
 import 'package:dendro3/domain/model/arbre.dart';
-import 'package:dendro3/domain/model/dispositif.dart';
-import 'package:dendro3/domain/model/dispositif_list.dart';
-import 'package:dendro3/domain/usecase/download_dispositif_data_usecase.dart';
 
 abstract class ArbresRepository {
   Future<Arbre> insertArbre(
