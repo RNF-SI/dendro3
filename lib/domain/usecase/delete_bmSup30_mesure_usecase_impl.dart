2a363b991d368089c1e06ec079e69f03fd802248
aschlegle <antoine.schlegel01@gmail.com>
2023-12-07 10:47:04 +0100

    Delete Bms mesure usecase

--- lib/domain/usecase/delete_bmSup30_mesure_usecase_impl.dart
+++ lib/domain/usecase/delete_bmSup30_mesure_usecase_impl.dart
@@ -1,15 +1,13 @@
 import 'package:dendro3/domain/repository/bmsSup30_mesures_repository.dart';
 import 'package:dendro3/domain/usecase/delete_bmSup30_mesure_usecase.dart';
 
-class DeleteBmSup30AndMesureUseCaseImpl implements DeleteBmSup30MesureUseCase {
-  final BmsSup30MesuresRepository bmsup30MesureRepositoryMesure;
+class DeleteBmSup30MesureUseCaseImpl implements DeleteBmSup30MesureUseCase {
+  final BmsSup30MesuresRepository _bmSup30MesuresRepository;
 
-  DeleteBmSup30AndMesureUseCaseImpl(
-    this.bmsup30MesureRepositoryMesure,
-  );
+  DeleteBmSup30MesureUseCaseImpl(this._bmSup30MesuresRepository);
 
   @override
   Future<void> execute(int id) async {
-    await bmsup30MesureRepositoryMesure.deleteBmsSup30Mesure(id);
+    await _bmSup30MesuresRepository.deleteBmsSup30Mesure(id);
   }
 }
