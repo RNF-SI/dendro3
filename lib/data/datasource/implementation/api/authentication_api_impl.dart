c62e898aa2fb47729da7bf35ccf30fdbe8dbf964
aschlegle <antoine.schlegel01@gmail.com>
2023-12-19 16:31:10 +0100

    http for debug on pixel

--- lib/data/datasource/implementation/api/authentication_api_impl.dart
+++ lib/data/datasource/implementation/api/authentication_api_impl.dart
@@ -8,7 +8,10 @@ import 'package:sqflite/sqflite.dart';
 import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 
-const String apiBase = "http://10.0.2.2:8000";
+// const String apiBase = "http://10.0.2.2:8000";
+const String apiBase = "http://192.168.1.64:8000";
+// const String apiBase = "https://192.168.1.54:8000";
+// const String apiBase = "https://192.168.122.1:8000";
 
 class AuthenticationApiImpl implements AuthenticationApi {
   @override
