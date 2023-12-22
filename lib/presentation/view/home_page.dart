02f06d273f8ffd0cc753f35e91279159a6978309
aschlegle <antoine.schlegel01@gmail.com>
2023-12-22 11:56:25 +0100

    Stay connected + Login and Logout

--- lib/presentation/view/home_page.dart
+++ lib/presentation/view/home_page.dart
@@ -1,5 +1,7 @@
 // import 'package:authentication_riverpod/providers/auth_provider.dart';
+import 'package:dendro3/presentation/view/login_page.dart';
 import 'package:dendro3/presentation/view/user_dispositif_list.dart';
+import 'package:dendro3/presentation/viewmodel/auth/auth_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -8,7 +10,23 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final authViewModel = ref.read(authenticationViewModelProvider);
+
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Mes Dispositifs"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await authViewModel.signOut();
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => LoginPage(),
+              ));
+            },
+          ),
+        ],
+      ),
       body: SafeArea(
         child: Column(
           children: const [
