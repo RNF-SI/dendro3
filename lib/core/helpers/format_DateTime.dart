2eabc8ed9042df8d7c54c0739b2f1c08ee9114c8
aschlegle <antoine.schlegel01@gmail.com>
2024-01-19 15:10:14 +0100

    Format datetime with utc

--- lib/core/helpers/format_DateTime.dart
+++ lib/core/helpers/format_DateTime.dart
@@ -1,9 +1,10 @@
 String formatDateTime(DateTime dateTime) {
-  return "${dateTime.year.toString().padLeft(4, '0')}-"
-      "${dateTime.month.toString().padLeft(2, '0')}-"
-      "${dateTime.day.toString().padLeft(2, '0')} "
-      "${dateTime.hour.toString().padLeft(2, '0')}:"
-      "${dateTime.minute.toString().padLeft(2, '0')}:"
-      "${dateTime.second.toString().padLeft(2, '0')}."
-      "${dateTime.millisecond.toString().padLeft(3, '0')}";
+  DateTime utcDateTime = dateTime.toUtc();
+  return "${utcDateTime.year.toString().padLeft(4, '0')}-"
+      "${utcDateTime.month.toString().padLeft(2, '0')}-"
+      "${utcDateTime.day.toString().padLeft(2, '0')} "
+      "${utcDateTime.hour.toString().padLeft(2, '0')}:"
+      "${utcDateTime.minute.toString().padLeft(2, '0')}:"
+      "${utcDateTime.second.toString().padLeft(2, '0')}."
+      "${utcDateTime.millisecond.toString().padLeft(3, '0')}";
 }
