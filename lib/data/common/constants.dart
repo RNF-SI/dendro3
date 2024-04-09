57f1af21d3ba0f361f126ce49e679a5840ac4317
aschlegle <antoine.schlegel01@gmail.com>
2024-04-09 11:45:37 +0200

    Precision sur la constante apiBase

--- lib/data/common/constants.dart
+++ lib/data/common/constants.dart
@@ -1,2 +1,4 @@
+// In case we are working with an android emulator
 // const String apiBase = "http://10.0.2.2:8000";
+// In the case we are working with a physical device
 const String apiBase = "http://192.168.1.15:8000";
