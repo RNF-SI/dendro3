711209e9de5755aa09973c51996945f19743aac1
aschlegle <antoine.schlegel01@gmail.com>
2024-04-17 18:12:05 +0200

    Improve synchronisation display

--- lib/presentation/view/sync_result.dart
+++ lib/presentation/view/sync_result.dart
@@ -2,6 +2,8 @@ import 'package:dendro3/core/helpers/sync_objects.dart';
 import 'package:dendro3/presentation/viewmodel/dispositif/sync_state_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SyncResultsWidget extends ConsumerWidget {
   final int dispositifId;
@@ -13,85 +15,104 @@ class SyncResultsWidget extends ConsumerWidget {
     final syncState = ref.watch(syncStateProvider(dispositifId));
 
     return Scaffold(
-      appBar: AppBar(title: Text("Synchronisation")),
+      appBar: AppBar(title: Text("Synchronization")),
       body: syncState.isLoading
           ? Center(child: CircularProgressIndicator())
           : syncState.error != null
               ? Text("Error: ${syncState.error}")
-              : _buildSyncResults(syncState.results),
+              : _buildSyncResults(context, syncState.results),
     );
   }
 
-  Widget _buildSyncResults(SyncResults? results) {
+  Widget _buildSyncResults(BuildContext context, SyncResults? results) {
     if (results == null) {
       return Text("No synchronization data available.");
     } else {
       return SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Text("Local Arbres:"),
-            // Text("  Created: ${results.localArbres.created}"),
-            // Text("  Updated: ${results.localArbres.updated}"),
-            // Text("  Deleted: ${results.localArbres.deleted}"),
-            // Divider(),
-            Text("Distant Arbres:"),
-            Text("  Created: ${results.distantArbres.created}"),
-            Text("  Updated: ${results.distantArbres.updated}"),
-            Text("  Deleted: ${results.distantArbres.deleted}"),
-            Divider(),
-            // Text("Local Arbres Mesures:"),
-            // Text("  Created: ${results.localArbresMesures.created}"),
-            // Text("  Updated: ${results.localArbresMesures.updated}"),
-            // Text("  Deleted: ${results.localArbresMesures.deleted}"),
-            // Divider(),
-            Text("Distant Arbres Mesures:"),
-            Text("  Created: ${results.distantArbresMesures.created}"),
-            Text("  Updated: ${results.distantArbresMesures.updated}"),
-            Text("  Deleted: ${results.distantArbresMesures.deleted}"),
-            Divider(),
-            // Text("Local Bms:"),
-            // Text("  Created: ${results.localBms.created}"),
-            // Text("  Updated: ${results.localBms.updated}"),
-            // Text("  Deleted: ${results.localBms.deleted}"),
-            // Divider(),
-            Text("Distant Bms:"),
-            Text("  Created: ${results.distantBms.created}"),
-            Text("  Updated: ${results.distantBms.updated}"),
-            Text("  Deleted: ${results.distantBms.deleted}"),
-            Divider(),
-            // Text("Local Bms Mesures:"),
-            // Text("  Created: ${results.localBmsMesures.created}"),
-            // Text("  Updated: ${results.localBmsMesures.updated}"),
-            // Text("  Deleted: ${results.localBmsMesures.deleted}"),
-            // Divider(),
-            Text("Distant Bms Mesures:"),
-            Text("  Created: ${results.distantBmsMesures.created}"),
-            Text("  Updated: ${results.distantBmsMesures.updated}"),
-            Text("  Deleted: ${results.distantBmsMesures.deleted}"),
-            Divider(),
-            Text("Distant Reperes:"),
-            Text("  Created: ${results.distantReperes.created}"),
-            Text("  Updated: ${results.distantReperes.updated}"),
-            Text("  Deleted: ${results.distantReperes.deleted}"),
-            Divider(),
-            Text("Distant CorCyclesPlacettes:"),
-            Text("  Created: ${results.distantCorCyclesPlacettes.created}"),
-            Text("  Updated: ${results.distantCorCyclesPlacettes.updated}"),
-            Text("  Deleted: ${results.distantCorCyclesPlacettes.deleted}"),
-            Divider(),
-            Text("Distant Regenerations:"),
-            Text("  Created: ${results.distantRegenerations.created}"),
-            Text("  Updated: ${results.distantRegenerations.updated}"),
-            Text("  Deleted: ${results.distantRegenerations.deleted}"),
-            Divider(),
-            Text("Distant Transects:"),
-            Text("  Created: ${results.distantTransects.created}"),
-            Text("  Updated: ${results.distantTransects.updated}"),
-            Text("  Deleted: ${results.distantTransects.deleted}"),
-          ],
+          children: _buildListSections(results),
         ),
       );
     }
+  }
+
+  List<Widget> _buildListSections(SyncResults results) {
+    List<Widget> sections = [];
+    // Example for one section, repeat for others
+    // Section for CorCyclePlacettes
+    sections.add(_buildSection(
+      "CorCyclePlacettes",
+      results.distantCorCyclesPlacettes,
+      Icons.location_on, // Icon suggestive of specific locations (placettes)
+    ));
+    // Section for Distant Arbres
+    sections.add(_buildSection(
+      "Arbres",
+      results.distantArbres,
+      Icons.park,
+    ));
+
+    // Section for Arbres Mesures
+    sections.add(_buildSection(
+      "Arbres Mesures",
+      results.distantArbresMesures,
+      Icons.onetwothree, // Icon suggestive of a tree measured periodically
+    ));
+
+    // Section for Bms
+    sections.add(_buildSection(
+      "Bms",
+      results.distantBms,
+      Icons.compost, // Icon suggestive of dead wood in a forest
+    ));
+
+    // Section for Bms Mesures
+    sections.add(_buildSection(
+      "Bms Mesures",
+      results.distantBmsMesures,
+      Icons.onetwothree, // Icon suggestive of measured data for dead wood
+    ));
+
+    // Section for Transects
+    sections.add(_buildSection(
+      "Transects",
+      results.distantTransects,
+      Icons.linear_scale, // Icon suggestive of measurements of small dead wood
+    ));
+    sections.add(_buildSection(
+      "Regenerations",
+      results.distantRegenerations,
+      Icons.child_care, // Icon suggestive of measurements of small dead wood
+    ));
+    // Section for Reperes
+    sections.add(_buildSection(
+      "Reperes",
+      results.distantReperes,
+      Icons.visibility, // Icon suggestive of observations
+    ));
+    // Add other sections similarly
+    return sections;
+  }
+
+  Widget _buildSection(String title, SyncDetails data, IconData icon) {
+    return ExpansionTile(
+      title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+      leading: Icon(icon),
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.add_circle_outline, color: Colors.green),
+          title: Text("Created: ${data.created}"),
+        ),
+        ListTile(
+          leading: Icon(Icons.update, color: Colors.amber),
+          title: Text("Updated: ${data.updated}"),
+        ),
+        ListTile(
+          leading: Icon(Icons.delete_outline, color: Colors.red),
+          title: Text("Deleted: ${data.deleted}"),
+        ),
+      ],
+    );
   }
 }
