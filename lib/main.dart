a7b34eb07595189f95524d5e3aef32bed4cf0933
aschlegle <antoine.schlegel01@gmail.com>
2024-04-20 10:46:36 +0200

    Material color created in main to change material green

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,27 @@ final _router = GoRouter(
   ],
 );
 
+// Function to create MaterialColor
+MaterialColor createMaterialColor(Color color) {
+  List strengths = <double>[.05];
+  final swatch = <int, Color>{};
+  final int r = color.red, g = color.green, b = color.blue;
+
+  for (int i = 1; i < 10; i++) {
+    strengths.add(0.1 * i);
+  }
+  for (var strength in strengths) {
+    final double ds = 0.5 - strength;
+    swatch[(strength * 1000).round()] = Color.fromRGBO(
+      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+      1,
+    );
+  }
+  return MaterialColor(color.value, swatch);
+}
+
 void main() async {
   FlutterError.demangleStackTrace = (StackTrace stack) {
     if (stack is stack_trace.Trace) return stack.vmTrace;
@@ -59,6 +80,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final MaterialColor customBlueSwatch = createMaterialColor(
+      Color(0xFF8AAC3E),
+    );
+
     return MaterialApp.router(
       routerConfig: _router,
       theme: ThemeData(
@@ -97,7 +122,7 @@ class MainApp extends StatelessWidget {
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
           ),
         ),
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.green)
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: customBlueSwatch)
             .copyWith(secondary: Color(0xFF8AAC3E)),
       ),
     );
